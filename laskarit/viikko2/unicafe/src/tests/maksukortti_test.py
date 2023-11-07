0586b3a8f50000f81c350555634f8d9ffe57694d
Topias Muukkonen <topias.muukkonen@gmail.com>
2023-11-07 18:01:54 +0200

    ekat testit unicafe

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,25 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_on_alussa_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10)
+
+    def test_rahan_lisaaminen_kasvattaa_saldoa(self):
+        self.maksukortti.lataa_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 15)
+
+    def test_rahan_ottaminen_pienentaa_saldoa(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 5)
+
+    def test_rahan_ottaminen_ei_vaikuta_saldon_jos_ei_rahaa(self):
+        self.maksukortti.ota_rahaa(1500)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10)
+
+    def test_rahan_ottaminen_palauttaa_true_jos_tarpeeksi_rahaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+    
+    def test_rahan_ottaminen_palauttaa_false_jos_ei_tarpeeksi_rahaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1500), False)
+    
